efe6bb897db047dc608d10a9d2555e9ff66b3858
JoshAtticus <72828296+JoshAtticus@users.noreply.github.com>
2022-12-05 10:01:10 +0800

    Alright that was too bad

--- src/App.svelte
+++ src/App.svelte
@@ -123,7 +123,7 @@
 		background-color: var(--background);
 		color: var(--foreground);
 		scrollbar-color: var(--orange) var(--orange-scrollbar-back);
-		font-size: 1pt;
+		font-size: 3pt;
 	}
 
 	#main.theme-orange.mode-dark {
