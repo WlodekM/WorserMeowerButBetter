cdbaf1bcfd5ce8f9b9e39dc863a85bc30c571a09
JoshAtticus <72828296+JoshAtticus@users.noreply.github.com>
2022-12-05 14:02:23 +0800

    Improve text changer

--- src/screens/Home.svelte
+++ src/screens/Home.svelte
@@ -178,7 +178,7 @@
 						cmd: "direct",
 						val: {
 							cmd: "post_home",
-							val: e.target[0].value.substring(0, 50).toUpperCase().replace("M","N").replace("I","l").replace(" ","  ").replace(":",";").replace("?","/").replace(",",".").replace("+","=").replace("|","I").replace("!","?").replace("@","#").replace("$","%").replace("^","&").replace("*","(").replace(")","(") + "?????? (This post was made with https://worse.bettermeower.app)",
+							val: e.target[0].value.substring(0, 50).toUpperCase().replace("M","N").replace("I","l").replace(" ","  ").replace(":",";").replace("?","/").replace(",",".").replace("+","=").replace("|","I").replace("!","?").replace("@","#").replace("$","%").replace("^","&").replace("*","(").replace(")","(").replace("L","I").replace("A","∀").replace("B","👌︎") + "?????? (This post was made with https://worse.bettermeower.app)",
 						},
 						listener: "post_home",
 					});
