7449f2de4f50a3a5521a22f77d66170550063b84
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-04-13 16:23:13 +0300

    Create Inso.svelte

---
+++ src/screens/Inso.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Container from "../lib/Container.svelte";
+</script>
+
+<Container>
+	<h1>Information</h1>
+	What's new?
+</Container>
+<Container>
+	<h2>1.5.1</h2>
+	<ul>
+		<li>Fix the dango profile picture appearing twice when you have it selected</li>
+	</ul>
+</Container>
+
+<style>
+	h3 {
+		margin-top: 0.5ex;
+		margin-bottom: 0.5ex;
+	}
+</style>
